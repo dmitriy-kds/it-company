c705df5bb5521a64d4127945c316a51bd7ec6834
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-02-19 14:43:32 +0200

    feat: Add TaskType model

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class TaskType(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "task types"
+
+    def __str__(self):
+        return self.name
