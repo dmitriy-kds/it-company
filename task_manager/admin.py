7c9449c1e71ec49b97209c4b28c697348bfd1277
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-02-20 15:19:44 +0200

    feat: Register TaskTypeAdmin class

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from task_manager.models import TaskType, Position, Task, Worker
 
-# Register your models here.
+
+@admin.register(TaskType)
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    search_fields = ["name"]
