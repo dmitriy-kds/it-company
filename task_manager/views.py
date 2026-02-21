6995820dc6c546e3a1ae1c68a58657b987434b12
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-02-21 19:42:12 +0200

    feat: Add basic index view

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Task
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        "num_tasks": Task.objects.count(),
+        "num_new_tasks": Task.objects.filter(status="New").count(),
+        "num_completed_tasks": Task.objects.filter(status="Done").count(),
+        "num_in_progress_tasks": Task.objects.filter(status="Done").count(),
+    }
+    return render(request, template_name="index.html", context=context)
