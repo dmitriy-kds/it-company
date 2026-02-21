49e17f3963d81074e02e03853e5adc0075e7fb84
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-02-21 21:26:35 +0200

    feat: Create list & detail urls for Worker, Task, Position, TaskType models

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,28 @@
 
 from django.urls import path
 
-from task_manager.views import index
+from task_manager.views import (
+    index,
+    WorkerListView,
+    WorkerDetailView,
+    TaskListView,
+    TaskDetailView,
+    TaskTypeListView,
+    TaskTypeDetailView,
+    PositionListView,
+    PositionDetailView,
+)
 
 urlpatterns = [
     path("", index, name="index"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("workers/<int:pk>/", WorkerDetailView.as_view(), name="worker-detail"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("position/", PositionListView.as_view(), name="position-list"),
+    path("position/<int:pk>/", PositionDetailView.as_view(), name="position-detail"),
+    path("task-type/", TaskTypeListView.as_view(), name="task-type-list"),
+    path("task-type/<int:pk>/", TaskTypeDetailView.as_view(), name="task-type-detail"),
 ]
 
 app_name = "task_manager"
