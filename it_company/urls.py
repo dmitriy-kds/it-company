2ca1b0f910eeb566d11c74f13092cd7fef656100
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-02-24 23:36:06 +0200

    feat: Override accounts/profile/ url to a custom view

--- it_company/urls.py
+++ it_company/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from task_manager.views import profile_redirect
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("task_manager.urls", namespace="task_manager")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", profile_redirect, name="profile"),
 ]
