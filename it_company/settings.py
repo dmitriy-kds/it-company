db9fd84f6a30b13618f4b9ab44caaa6422bdde15
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-02-25 15:30:27 +0200

    feat: Add crispy_bootstrap5 to settings.py

--- it_company/settings.py
+++ it_company/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "task_manager",
-    "bootstrap4"
+    "bootstrap4",
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,10 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = "it_company.wsgi.application"
 
