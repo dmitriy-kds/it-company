72b6ba792c449ca2591eeec5615037f8d56a2c48
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-12 22:18:31 +0200

    style: Add return type to clean_deadline in forms.py

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,6 +1,6 @@
+from datetime import date
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
-
 from task_manager.models import Worker, Task
 
 
@@ -28,13 +28,13 @@ class TaskCreateForm(forms.ModelForm):
             "assignees",
         ]
 
-    def clean_deadline(self):
-        date = self.cleaned_data["deadline"]
-        if date.weekday() == 6:
+    def clean_deadline(self) -> date:
+        d = self.cleaned_data["deadline"]
+        if d.weekday() == 6:
             raise forms.ValidationError(
                 "We don't work on Sundays!"
             )
-        return date
+        return d
 
 
 class WorkerUpdateForm(UserChangeForm):
@@ -52,8 +52,16 @@ class WorkerUpdateForm(UserChangeForm):
 
 
 class TaskNameDescriptionSearchForm(forms.Form):
-    name_or_description = forms.CharField(max_length=50, required=False, label="")
+    name_or_description = forms.CharField(
+        max_length=50,
+        required=False,
+        label=""
+    )
 
 
 class WorkerFirstLastNameSearchForm(forms.Form):
-    first_or_last_name = forms.CharField(max_length=50, required=False, label="")
+    first_or_last_name = forms.CharField(
+        max_length=50,
+        required=False,
+        label=""
+    )
