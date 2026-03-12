e51587c4d3f3916a5036857bd3b477038ee76bb6
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-12 16:07:54 +0200

    style: Enforce max line length on test_index_views

--- task_manager/tests/test_index_view.py
+++ task_manager/tests/test_index_view.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from django.urls import reverse
-
 from task_manager.models import Worker, Position, Task
 
 
@@ -17,9 +16,24 @@ class IndexTests(TestCase):
 
     def test_index_context(self):
         response = self.client.get(reverse("task_manager:index"))
-        self.assertEqual(response.context["num_employees"], Worker.objects.count())
-        self.assertEqual(response.context["num_positions"], Position.objects.count())
-        self.assertEqual(response.context["num_tasks"], Task.objects.count())
-        self.assertEqual(response.context["num_new_tasks"], Task.objects.filter(status="New").count())
-        self.assertEqual(response.context["num_completed_tasks"], Task.objects.filter(status="Done").count())
-        self.assertEqual(response.context["num_in_progress_tasks"], Task.objects.filter(status="In Progress").count())
+        self.assertEqual(
+            response.context["num_employees"],
+            Worker.objects.count()
+        )
+        self.assertEqual(
+            response.context["num_positions"],
+            Position.objects.count()
+        )
+        self.assertEqual(
+            response.context["num_tasks"],
+            Task.objects.count()
+        )
+        self.assertEqual(
+            response.context["num_new_tasks"],
+            Task.objects.filter(status="New").count())
+        self.assertEqual(
+            response.context["num_completed_tasks"],
+            Task.objects.filter(status="Done").count())
+        self.assertEqual(
+            response.context["num_in_progress_tasks"],
+            Task.objects.filter(status="In Progress").count())
