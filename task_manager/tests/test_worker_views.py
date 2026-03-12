d7a6f23862408a26d821d0604ff29c2ba87a9623
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-12 21:57:43 +0200

    style: Enforce max line length on test_worker_views

--- task_manager/tests/test_worker_views.py
+++ task_manager/tests/test_worker_views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
+
 class PublicWorkerTests(TestCase):
     def setUp(self):
        self.worker = get_user_model().objects.create_user(
@@ -22,17 +23,26 @@ class PublicWorkerTests(TestCase):
         self.assertRedirects(response, reverse("login") + "?next=" + url)
 
     def test_worker_detail_login_required(self):
-        url = reverse("task_manager:worker-detail", kwargs={"pk": self.worker.pk})
+        url = reverse(
+            "task_manager:worker-detail",
+            kwargs={"pk": self.worker.pk}
+        )
         response = self.client.get(url)
         self.assertRedirects(response, reverse("login") + "?next=" + url)
 
     def test_worker_update_login_required(self):
-        url = reverse("task_manager:worker-update", kwargs={"pk": self.worker.pk})
+        url = reverse(
+            "task_manager:worker-update",
+            kwargs={"pk": self.worker.pk}
+        )
         response = self.client.get(url)
         self.assertRedirects(response, reverse("login") + "?next=" + url)
 
     def test_worker_delete_login_required(self):
-        url = reverse("task_manager:worker-delete", kwargs={"pk": self.worker.pk})
+        url = reverse(
+            "task_manager:worker-delete",
+            kwargs={"pk": self.worker.pk}
+        )
         response = self.client.get(url)
         self.assertRedirects(response, reverse("login") + "?next=" + url)
 
@@ -62,13 +72,19 @@ class PrivateWorkerTests(TestCase):
 
     def test_worker_list_search_by_first_name(self):
         url = reverse("task_manager:worker-list")
-        response = self.client.get(url, data={"first_or_last_name": self.another_worker.first_name})
+        response = self.client.get(
+            url,
+            data={"first_or_last_name": self.another_worker.first_name}
+        )
         self.assertContains(response, self.another_worker.last_name)
         self.assertNotContains(response, self.worker.last_name)
 
     def test_worker_list_search_by_last_name(self):
         url = reverse("task_manager:worker-list")
-        response = self.client.get(url, data={"first_or_last_name": self.another_worker.last_name})
+        response = self.client.get(
+            url,
+            data={"first_or_last_name": self.another_worker.last_name}
+        )
         self.assertContains(response, self.another_worker.first_name)
         self.assertNotContains(response, self.worker.first_name)
 
@@ -100,13 +116,21 @@ class PrivateWorkerTests(TestCase):
             "password1": "123413458ijnnl0*",
             "password2": "123413458ijnnl0*",
         }
-        response = self.client.post(reverse("task_manager:worker-create"), data=form_data)
-        new_user = get_user_model().objects.get(username=form_data["username"])
+        response = self.client.post(
+            reverse("task_manager:worker-create"),
+            data=form_data
+        )
+        new_user = get_user_model().objects.get(
+            username=form_data["username"]
+        )
         self.assertEqual(new_user.first_name, form_data["first_name"])
         self.assertEqual(new_user.last_name, form_data["last_name"])
         self.assertEqual(new_user.username, form_data["username"])
         self.assertEqual(new_user.email, form_data["email"])
-        self.assertRedirects(response, expected_url=reverse("task_manager:worker-list"))
+        self.assertRedirects(
+            response,
+            expected_url=reverse("task_manager:worker-list")
+        )
 
     def test_worker_create_post_with_invalid_data(self):
         form_data = {
@@ -117,18 +141,29 @@ class PrivateWorkerTests(TestCase):
             "password1": "1234",
             "password2": "1234",
         }
-        response = self.client.post(reverse("task_manager:worker-create"), data=form_data)
-        new_user = get_user_model().objects.filter(username=form_data["username"])
+        response = self.client.post(
+            reverse("task_manager:worker-create"),
+            data=form_data
+        )
+        new_user = get_user_model().objects.filter(
+            username=form_data["username"]
+        )
         self.assertFalse(new_user.exists())
         self.assertEqual(response.status_code, 200)
 
     def test_worker_update_returns_200(self):
-        url = reverse("task_manager:worker-update", kwargs={"pk": self.another_worker.pk})
+        url = reverse(
+            "task_manager:worker-update",
+            kwargs={"pk": self.another_worker.pk}
+        )
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_worker_update_get_is_pre_populated(self):
-        url = reverse("task_manager:worker-update", kwargs={"pk": self.another_worker.pk})
+        url = reverse(
+            "task_manager:worker-update",
+            kwargs={"pk": self.another_worker.pk}
+        )
         response = self.client.get(url)
         self.assertContains(response, self.another_worker.last_name)
 
@@ -136,29 +171,60 @@ class PrivateWorkerTests(TestCase):
         form_data = {
             "username": "updated",
         }
-        response = self.client.post(reverse("task_manager:worker-update", kwargs={"pk": self.another_worker.pk}), data=form_data)
+        response = self.client.post(
+            reverse(
+                "task_manager:worker-update",
+                kwargs={"pk": self.another_worker.pk}
+            ),
+            data=form_data
+        )
         self.another_worker.refresh_from_db()
-        self.assertEqual(self.another_worker.username, form_data["username"])
-        self.assertRedirects(response, expected_url=reverse("task_manager:worker-list"))
+        self.assertEqual(
+            self.another_worker.username,
+            form_data["username"]
+        )
+        self.assertRedirects(
+            response,
+            expected_url=reverse("task_manager:worker-list")
+        )
 
     def test_worker_delete_returns_200(self):
-        url = reverse("task_manager:worker-delete", kwargs={"pk": self.another_worker.pk})
+        url = reverse(
+            "task_manager:worker-delete",
+            kwargs={"pk": self.another_worker.pk}
+        )
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_worker_delete_post(self):
-        response = self.client.post(reverse("task_manager:worker-delete", kwargs={"pk": self.another_worker.pk}))
-        another_worker = get_user_model().objects.filter(username="another_username")
+        response = self.client.post(
+            reverse(
+                "task_manager:worker-delete",
+                kwargs={"pk": self.another_worker.pk}
+            )
+        )
+        another_worker = get_user_model().objects.filter(
+            username="another_username"
+        )
         self.assertFalse(another_worker.exists())
-        self.assertRedirects(response, expected_url=reverse("task_manager:worker-list"))
+        self.assertRedirects(
+            response,
+            expected_url=reverse("task_manager:worker-list")
+        )
 
     def test_worker_detail_returns_200(self):
-        url = reverse("task_manager:worker-detail", kwargs={"pk": self.worker.pk})
+        url = reverse(
+            "task_manager:worker-detail",
+            kwargs={"pk": self.worker.pk}
+        )
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
 
     def test_worker_detail_context(self):
-        url = reverse("task_manager:worker-detail", kwargs={"pk": self.worker.pk})
+        url = reverse(
+            "task_manager:worker-detail",
+            kwargs={"pk": self.worker.pk}
+        )
         response = self.client.get(url)
         self.assertIn("today", response.context)
         self.assertIn("new", response.context)
