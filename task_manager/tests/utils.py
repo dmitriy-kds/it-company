e8019ddbf6b094a9d863601edf3dd72026c56522
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-12 22:02:55 +0200

    style: Add return type to test utils functions

--- task_manager/tests/utils.py
+++ task_manager/tests/utils.py
@@ -1,13 +1,13 @@
 from datetime import date, timedelta
 
 
-def get_future_weekday():
+def get_future_weekday() -> date:
     d = date.today() + timedelta(days=1)
     while d.weekday() >= 5:
         d += timedelta(days=1)
     return d
 
-def get_future_sunday():
+def get_future_sunday() -> date:
     d = date.today() + timedelta(days=1)
     while d.weekday() != 6:
         d += timedelta(days=1)
