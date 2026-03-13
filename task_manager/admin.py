4d227a2ede02636a7bb4e34f487dee2ddfd252c9
Dmitriy Kuzmin <dmitriykuzmin@ymail.com>
2026-03-13 16:23:24 +0200

    style: Enforce PEP 8 rules on imports in admin.py

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from task_manager.models import TaskType, Position, Task, Worker
+
+from task_manager.models import (
+    TaskType,
+    Position,
+    Task,
+    Worker
+)
 
 
 @admin.register(TaskType)
